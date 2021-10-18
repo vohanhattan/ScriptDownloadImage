f44897555bde5ec8abc32c7400d2fb50f1f4bdf1
Nhat Tan <67008662+vohanhattan@users.noreply.github.com>
2021-10-18 14:50:24 +0700

    Add files via upload

---
+++ get_images.py
@@ -0,0 +1,21 @@
+import sys
+import urllib
+from csv import reader
+import os.path
+from urllib.request import urlretrieve
+
+csv_filename = sys.argv[1]
+
+with open(csv_filename+".csv".format(csv_filename), 'r') as csv_file:
+    for line in reader(csv_file):
+        if os.path.isfile("images/" + line[0] + ".jpg"):
+            print ("Image skipped for {0}".format(line[0]))
+        else:
+            if line[5] != '' and line[0] != "image_id":
+                try:
+                    urlretrieve(line[5], "images/" + line[0] + ".jpg")
+                    print ("Image saved for {0}".format(line[0]))
+                except Exception:
+                    pass
+            else:
+                print ("No result for {0}".format(line[0]))
